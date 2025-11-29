7d2a6ace6d6d9a915fe2da5633a5d07538290c7a
Olaitan <olaitanmaryakano@gmail.com>
2025-11-29 01:06:54 +0100

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,17 @@
-// prisma.config.ts
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-    datasource: {
-      url: process.env.DATABASE_URL!,
-    },
-});
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    seed: 'tsx prisma/seed.ts',
+  },
+  datasource: {
+    // Prefer DIRECT TCP via DATABASE_URL
+    url: env('DATABASE_URL'),
+    // Optionally support shadow DB if present:
+    // shadowDatabaseUrl: env('SHADOW_DATABASE_URL'),
+  },
+})
 
