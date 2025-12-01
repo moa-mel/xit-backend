e7edc35f7c4737f2660993746b53304971f5ddca
Olaitan <olaitanmaryakano@gmail.com>
2025-12-01 23:44:53 +0100

    fix error with regards to prisma 7

--- src/modules/core/prisma/services/index.ts
+++ src/modules/core/prisma/services/index.ts
@@ -1,9 +1,27 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'; 
+import { Pool } from 'pg';
 import logger from 'moment-logger';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+   constructor() {
+    // Create a PostgreSQL connection pool
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    // Create the Prisma adapter
+    const adapter = new PrismaPg(pool);
+
+    // Initialize PrismaClient with the adapter
+    super({
+      adapter,
+    });
+  }
+
   async onModuleInit() {
     logger.log('Connecting to the database...');
     await this.$connect();
