f17d9481ce836a7092e1cae18f1c1ef20cffb6cd
Olaitan <olaitanmaryakano@gmail.com>
2026-03-17 12:57:03 +0100

    add to module

--- src/modules/api/podcast/index.ts
+++ src/modules/api/podcast/index.ts
@@ -4,11 +4,13 @@ import { PodCastService } from "./services";
 import { BullModule } from "@nestjs/bull";
 import { BULL_QUEUES } from "@/bull/constants";
 import { NotificationModule } from "../notification";
+import { AIDetectionModule } from "../aiDetection";
 
 
 @Module({
   imports: [
     NotificationModule,
+    AIDetectionModule,
     BullModule.registerQueue({
       name: BULL_QUEUES.NOTIFICATION,
     }),
