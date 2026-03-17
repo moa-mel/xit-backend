94f2aba23e6b5832a51be2a9ec2a438f50ae368e
Olaitan <olaitanmaryakano@gmail.com>
2026-03-17 11:52:06 +0100

    fix dto error

--- src/modules/api/podcast/dtos/index.ts
+++ src/modules/api/podcast/dtos/index.ts
@@ -1,4 +1,5 @@
-import { IsBooleanString, IsNumber, IsNumberString, IsOptional, IsString, IsBoolean, IsDate, Type } from "class-validator"
+import { Type } from "class-transformer";
+import { IsBooleanString, IsNumber, IsNumberString, IsOptional, IsString, IsBoolean, IsDate } from "class-validator"
 
 export class ListenToPodcastDto {
     @IsNumber()
